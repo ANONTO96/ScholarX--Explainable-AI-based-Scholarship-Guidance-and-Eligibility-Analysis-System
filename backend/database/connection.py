087fb1e198551e288f13a516bc2731e14dae1ab0
abdullah al nafiu <abdullahall.nafiu@gmail.com>
2026-09-15 01:47:54 +0600

    MySQL Testing On Railway

--- backend/database/connection.py
+++ backend/database/connection.py
@@ -1,10 +1,27 @@
-import mysql.connector 
+# import mysql.connector 
+
+# def get_db_connection():
+#     database_config = {
+#         "host": "localhost",
+#         "user": "root",
+#         "password": "",
+#         "database": "study_abroad_ai"
+#     }
+#     return mysql.connector.connect(**database_config) # ** unpacks the dictionary into keyword arguments for the connect function
+
+
+
+import os
+import mysql.connector
+
 
 def get_db_connection():
     database_config = {
-        "host": "localhost",
-        "user": "root",
-        "password": "",
-        "database": "study_abroad_ai"
+        "host": os.getenv("MYSQLHOST"),
+        "port": int(os.getenv("MYSQLPORT")),
+        "user": os.getenv("MYSQLUSER"),
+        "password": os.getenv("MYSQLPASSWORD"),
+        "database": os.getenv("MYSQLDATABASE"),
     }
-    return mysql.connector.connect(**database_config) # ** unpacks the dictionary into keyword arguments for the connect function
+
+    return mysql.connector.connect(**database_config)
